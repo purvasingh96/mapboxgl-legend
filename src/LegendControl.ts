c69d73b91484b11b922089b8c257f2c1208f5427
psingh359 <singhpurva2906@gmail.com>
2023-04-13 15:48:06 -0400

    Corrects aria-label usage

--- src/LegendControl.ts
+++ src/LegendControl.ts
@@ -86,7 +86,10 @@ export default class LegendControl implements IControl {
   private _toggleButton(layerId: string, layerKey: string) {
     const { onToggle = this._options.onToggle } = this._options.layers[layerKey] || {};
     const visibility = this._map?.getLayoutProperty(layerId, 'visibility') || 'visible';
-    const button = createElement('button', { classes: ['toggler', `toggler--${visibility}`] });
+    const buttonProps = {
+      'aria-label': 'Toggle data layer on map'
+    };
+    const button = createElement('button',{ ...buttonProps, classes: ['toggler', `toggler--${visibility}`] });
     button.addEventListener('click', event => {
       event.preventDefault();
       const visible = visibility === 'none' ? 'visible' : 'none';
